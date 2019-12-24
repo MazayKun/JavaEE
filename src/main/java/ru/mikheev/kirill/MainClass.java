45378d664ce91390a64e6d7914b3a939ae501cbc
MazayKun <kir99916@gmail.com>
2019-12-24 16:43:29 +0300

    Initial commit

---
+++ src/main/java/ru/mikheev/kirill/MainClass.java
@@ -0,0 +1,12 @@
+package ru.mikheev.kirill;
+
+/**
+ * @author Kirill Mikheev
+ * @version 1.0
+ */
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Puff");
+    }
+}
